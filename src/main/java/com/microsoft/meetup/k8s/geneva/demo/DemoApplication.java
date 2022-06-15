cba577d7798b3f1cf018a9e89f499dff09a83677
Dominique Broeglin <dominique.broeglin@microsoft.com>
2022-06-15 18:04:53 +0200

    Adding a web endpoint to the application

--- src/main/java/com/microsoft/meetup/k8s/geneva/demo/DemoApplication.java
+++ src/main/java/com/microsoft/meetup/k8s/geneva/demo/DemoApplication.java
@@ -2,12 +2,20 @@ package com.microsoft.meetup.k8s.geneva.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+
+	@RequestMapping("/")
+	public String index() {
+		return "Welcome to Kubernetes Geneva\n";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
